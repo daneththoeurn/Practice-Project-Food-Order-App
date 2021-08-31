c860d89db93c5ae7ef280c52120ceebdaec143ee
daneththoeurn <88901303+daneththoeurn@users.noreply.github.com>
2021-09-01 01:26:24 +0700

    finished Meals

---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MealsSummary from "./MealSummary";
+import AvailableMeals from "./AvailableMeals";
+
+function Meals() {
+  return (
+    <div>
+      <MealsSummary />
+      <AvailableMeals />
+    </div>
+  );
+}
+
+export default Meals;
